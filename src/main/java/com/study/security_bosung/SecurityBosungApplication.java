715d077ad22c46e786ede81e055982547db1303a
qhtjd8989 <qhtjd8899@naver.com>
2022-08-08 19:13:54 +0900

    springboot security 시작

---
+++ src/main/java/com/study/security_bosung/SecurityBosungApplication.java
@@ -0,0 +1,13 @@
+package com.study.security_bosung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBosungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityBosungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/security_bosung/SecurityBosungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.security_bosung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityBosungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
